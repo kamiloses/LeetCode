fb214c870e2a9b6263d0dd53007648a6805ef410
kamiloses <kamil.kurzaj04@gmail.com>
2025-10-16 11:28:26 +0200

    1

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿public class Solution{
+public string LongestCommonPrefix(string[] strs) {
+    if (strs == null || strs.Length == 0) return "";
 
-Console.WriteLine("Hello, World!");
+    string currentPrefix = "";
+    int maxLength = strs.Min(s => s.Length); 
+
+    for (int i = 0; i < maxLength; i++) {
+        char currentChar = strs[0][i];
+        for (int j = 1; j < strs.Length; j++) {
+            if (strs[j][i] != currentChar) {
+                return currentPrefix; 
+            }
+        }
+        currentPrefix += currentChar; 
+    }
+
+    return currentPrefix;
+}
+
+public static void Main(string[] args)
+{
+    var solution = new Solution();
+   Console.WriteLine( solution.LongestCommonPrefix(new string[]{"flower","flow","flight"}));
+}
+}
