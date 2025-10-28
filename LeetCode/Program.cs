9444f233be141b88594fb491b01a2fba91362b4e
kamiloses <kamil.kurzaj04@gmail.com>
2025-10-28 11:57:24 +0100

    5

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,23 +1,116 @@
-﻿public class Solution
+﻿using System.Text;
+
+public class Sorting
 {
-    public static int points(String[] games)
+
+
+
+    int[] BubbleSort(int[] nums)
     {
-        int result = 0;
-        foreach (var game in games)
+
+        for (int i = 0; i < nums.Length - 1; i++)
         {
-            int[] scores = game.Split(":").Select(x => int.Parse(x)).ToArray();
-            if (scores[0] > scores[1]) result += 3;
-            else if (scores[0] == scores[1]) result += 1;
-            else result += 0;
+            for (int j = 0; j < nums.Length - i - 1; j++)
+            {
+                if (nums[j] > nums[j + 1])
+                {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
         }
 
-        return result;
+        return nums;
     }
 
 
-    public static void Main(string[] args)
+
+    public int[] SelectionSort(int[] nums) //https://www.youtube.com/watch?v=EwjnF7rFLns&t=321s
     {
-        Object solution = new Solution();
-        Console.WriteLine(points(new string[] { "3:1", "2:2", "0:1" }));
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            int minIndex = i; // zakładamy, że najmniejszy element jest na pozycji i
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[j] < nums[minIndex])
+                {
+                    minIndex = j; // zapisujemy indeks najmniejszego elementu
+                }
+            }
+
+            int temp = nums[i];
+            nums[i] = nums[minIndex];
+            nums[minIndex] = temp;
+        }
+
+
+        return nums;
     }
+
+
+
+    public class Leetcode
+    {
+        public bool IsPalindrome(string text)
+        {
+            var stringBuilder = new StringBuilder();
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                stringBuilder.Append(text[i]);
+
+            }
+
+            return stringBuilder.ToString().Equals(text);
+        }
+
+
+
+        public Dictionary<string, int> CountWordOccurrences(string text)
+        {
+            string[] arr = text.Split(" ");
+
+            var dictionary = new Dictionary<string, int>();
+
+            foreach (string word in arr)
+            {
+                if (!dictionary.ContainsKey(word))
+                {
+                    dictionary.Add(word, 1);
+                }
+                else
+                {
+                    dictionary[word]++;
+                }
+
+            }
+
+            return dictionary;
+        }
+    }
+
+
+    public int GreatestCommonDivisor(int a, int b)
+    {
+        int min = Math.Min(a, b);
+        int gcd = 1;
+
+        for (int i = 1; i <= min; i++)
+        {
+            if (a % i == 0 && b % i == 0)
+            {
+                gcd = i;
+            }
+        }
+
+        return gcd;
+    }
+
+
+
+   public static void Main(string[] args)
+   {
+       Leetcode leetcode = new Leetcode();
+       Console.Write(leetcode.CountWordOccurrences("ala ma kota i kot ma ale"));
+   }
 }
