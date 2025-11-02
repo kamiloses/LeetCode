d4ab165a378dc5a574f7489a5c4952160c124183
kamiloses <kamil.kurzaj04@gmail.com>
2025-11-02 10:17:48 +0100

    2)

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -52,28 +52,28 @@ public class Sorting
 
     public class Leetcode
     {
-        public void ReverseArray(int[] array)//odwróc tablice bez tworzenia nowej
+        public void ReverseArray(int[] array) //odwróc tablice bez tworzenia nowej
         {
             int temp;
-            for (int i = 0; i < array.Length/2; i++)//WAZNE JEST TO /2
+            for (int i = 0; i < array.Length / 2; i++) //WAZNE JEST TO /2
             {
-                temp=array[i];
-                array[i]= array[array.Length - 1 - i];
-                array[array.Length - 1 - i]=temp;
+                temp = array[i];
+                array[i] = array[array.Length - 1 - i];
+                array[array.Length - 1 - i] = temp;
             }
 
-          
+
         }
 
         public int FindMax(int[] array)
         {
-            if(array==null ||array.Length==0) throw new Exception("Array is empty");
-            
+            if (array == null || array.Length == 0) throw new Exception("Array is empty");
+
             int max = array[0];
             for (var i = 1; i < array.Length; i++)
             {
-                if (array[i] > max) max=array[i];
-       
+                if (array[i] > max) max = array[i];
+
             }
 
             return max;
@@ -81,13 +81,13 @@ public class Sorting
 
         public int FindMin(int[] array)
         {
-            
-            if(array==null ||array.Length==0) throw new Exception("Array is empty");
-            
+
+            if (array == null || array.Length == 0) throw new Exception("Array is empty");
+
             int min = array[0];
             for (int i = 1; i < array.Length; i++)
             {
-                if (min>array[i]) min=array[i];
+                if (min > array[i]) min = array[i];
             }
 
             return min;
@@ -95,8 +95,8 @@ public class Sorting
 
         public int SumArray(int[] array)
         {
-            if(array==null ||array.Length==0) throw new Exception("Array is empty");
-            
+            if (array == null || array.Length == 0) throw new Exception("Array is empty");
+
             int sum = 0;
             foreach (var i in array)
             {
@@ -108,26 +108,122 @@ public class Sorting
 
         public double averageArray(int[] array)
         {
-            if(array==null ||array.Length==0) throw new Exception("Array is empty");
-            
+            if (array == null || array.Length == 0) throw new Exception("Array is empty");
+
             int sum = 0;
             foreach (var i in array)
             {
                 sum += i;
             }
 
-            return (double)sum/array.Length;
+            return (double)sum / array.Length;
+        }
+
+        public int[] RemoveDuplicates(int[] array) //todo ogarnij
+        {
+            if (array == null || array.Length == 0)
+                return new int[0];
+
+            int uniqueCount = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool isDuplicate = false;
+                for (int j = 0; j < uniqueCount; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate)
+                {
+                    array[uniqueCount] = array[i];
+                    uniqueCount++;
+                }
+            }
+
+            int[] result = new int[uniqueCount];
+            Array.Copy(array, result, uniqueCount);
+            return result;
         }
 
 
+        public int[] FindUniqueElements(int[] array) //todo ogarnij
+        {
+            if (array == null || array.Length == 0)
+                throw new Exception("Array is empty");
 
+            int uniqueCount = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int occurrences = 0;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                        occurrences++;
+                }
+
+                if (occurrences == 1)
+                {
+                    array[uniqueCount] = array[i];
+                    uniqueCount++;
+                }
+            }
+
+            int[] result = new int[uniqueCount];
+            Array.Copy(array, result, uniqueCount);
+            return result;
+        }
+
+
+        public bool IsPalindrome(int[] array)
+        {
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                if (array[i] != array[array.Length - 1 - i])
+                    return false;
+            }
+            return true;
+        }
+
+        public void MoveZerosToEnd(int[] array) //todo ogarnij
+        {
+            if (array == null || array.Length == 0)
+                return;
+
+            int position = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != 0)
+                {
+                    array[position] = array[i];
+                    position++;
+                }
+            }
+
+            for (int i = position; i < array.Length; i++)
+            {
+                array[i] = 0;
+            }
+        }
+
+        //stringi
+        
+        
+
+
+
+
+
+        public static void Main(string[] args)
+        {
+            var leetcode = new Leetcode();
+            int[] result =leetcode.RemoveDuplicates([1, 1, 2, 2, 3]);
+            Console.WriteLine(string.Join(", ", result));
+        }
     }
-
-
-
-    public static void Main(string[] args)
-   {
-       var leetcode = new Leetcode();
-       leetcode.FindMin([1, 2, 3]);
-   }
 }
