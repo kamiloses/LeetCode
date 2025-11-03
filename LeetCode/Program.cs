9233e1c4786d59fe54fea3ab8d0dbeb2f8fa6b14
kamiloses <kamil.kurzaj04@gmail.com>
2025-11-03 12:46:45 +0100

    2)

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -428,7 +428,21 @@ public class Sorting
 
               return original == reversed;
           }
-          
+          public static bool IsPerfectNumber(int number)
+          {
+              if (number <= 1)
+                  return false;
+
+              int sum = 0;
+
+              for (int i = 1; i <= number / 2; i++)
+              {
+                  if (number % i == 0)
+                      sum += i;
+              }
+
+              return sum == number;
+          }
           
           
           
